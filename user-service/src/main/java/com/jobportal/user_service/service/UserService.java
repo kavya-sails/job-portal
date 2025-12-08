8a0a4c139427a23feb1e88fd5a126961a2447f33
pavankalyan015 <pavankalyan.penumajji@sailssoftware.com>
2025-12-08 21:21:07 +0530

    deleted  un necessary files

--- user-service/src/main/java/com/jobportal/user_service/service/UserService.java
+++ user-service/src/main/java/com/jobportal/user_service/service/UserService.java
@@ -1,4 +1,22 @@
 package com.jobportal.user_service.service;
 
+
+import com.jobportal.user_service.repository.UserDataRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private UserDataRepository userDataRepository;
+
+    void createUserProfile(){}
+
+    void getUserProfileById(){}
+
+    void getAllUserProfiles(){}
+
+    void deleteUserProfileById(){}
+
+    void updateUserProfile(){}
+
 }
