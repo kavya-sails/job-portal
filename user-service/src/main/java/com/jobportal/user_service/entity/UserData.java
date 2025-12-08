7a48e1883bdedb5b0e6d66b0bb0323c2faf6c2d6
DHANUSH-DODDI <dhanush.doddi@sailssoftware.com>
2025-12-08 14:54:46 +0530

    first commit

---
+++ user-service/src/main/java/com/jobportal/user_service/entity/UserData.java
@@ -0,0 +1,59 @@
+package com.jobportal.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //  Basic Profile Info
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 10)
+    private String phone;
+
+    private String skills;
+
+    //  Resume Upload Fields
+    private String resumeUrl;
+
+    private Long resumeSize;
+
+    private LocalDateTime resumeUploadedAt;
+
+    //  Soft Delete (Deactivate Account)
+    private Boolean isActive = true;
+
+    //  Auditing
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    //  Auto Timestamp Handling
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
