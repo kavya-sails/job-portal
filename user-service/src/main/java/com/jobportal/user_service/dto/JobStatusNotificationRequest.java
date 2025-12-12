05b0503745133acb2edc7017d1f7ec7ecac9bda5
kavyaNamballa <kavya.namballa@sailssoftware.com>
2025-12-12 14:01:48 +0530

    fix/deque the rejected events in the listener

--- user-service/src/main/java/com/jobportal/user_service/dto/JobStatusNotificationRequest.java
+++ user-service/src/main/java/com/jobportal/user_service/dto/JobStatusNotificationRequest.java
@@ -1,23 +1,19 @@
 package com.jobportal.user_service.dto;
 
-import jakarta.validation.constraints.NotBlank;
+import com.jobportal.user_service.enums.ApplicationStatus;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class JobStatusNotificationRequest {
-
     @NotNull
-    private Long userId;   // This matches UserData.id (for now)
-
+    private Long userId;
     @NotNull
     private Long jobId;
-
-    @NotBlank
-    private String status;  // ACCEPTED or REJECTED
+    @NotNull
+    private ApplicationStatus status;
 }
