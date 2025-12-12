9f3771e07d1bb572572c19d456c4e0838512e211
likhitha-sails <likhitha.nartu@sailssoftware.com>
2025-12-12 12:33:24 +0530

    userClient url updated

--- job-service/src/main/java/com/job_portal/job_service/client/UserClient.java
+++ job-service/src/main/java/com/job_portal/job_service/client/UserClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
         configuration = FeignClientConfig.class
 )
 public interface UserClient {
-    @GetMapping("/api/users/{userId}")
+    @GetMapping("/api/users/profile/apply/{userId}")
     void checkUserExists(@PathVariable("userId") Long userId);
 }
